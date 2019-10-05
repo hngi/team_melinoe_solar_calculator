3f025b4ac242b08ad6c60717c11cac9627d2abf2
rita008 <48980821+rita008@users.noreply.github.com>
2019-10-05 19:10:24 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,35 +3,35 @@ import "./Contact.css";
 class Contact extends React.Component {
   render() {
     return (
-     <div class="container">
+     <div className="container">
 
 
-        <div class="page-header">
+        <div className="page-header">
            <h1>CONTACT US</h1>
-           <p class="lead"><i> We are one mail away and we would love to hear from you.</i></p>
+           <p className="lead"><i> We are one mail away and we would love to hear from you.</i></p>
         </div>
         <br> <br> <hr>
 
 
-        <div class="row">
-            <div class="col-sm-6">
+        <div className="row">
+            <div className="col-sm-6">
     <form>
-        <div class="form-group">
+        <div className="form-group">
           <label for="formName">Name</label>
-          <input type="text" class="form-control" id="formName" placeholder="enter your name">  
+          <input type="text" className="form-control" id="formName" placeholder="enter your name">  
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="your@email.com">
-         <div class="emailNote"> <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small></div>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="your@email.com">
+         <div className="emailNote"> <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small></div>
         </div>
 
-        <div class="form-group">
+        <div className="form-group">
             <label for="exampleFormControlTextarea1">Message</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
         
-        <button type="submit" class="btn btn-primary">Send message</button>
+        <button type="submit" className="btn btn-primary">Send message</button>
       </form>
     </div>
 
