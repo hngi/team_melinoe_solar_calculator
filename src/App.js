4a902f10ad56852e4f87e74c9282445b74f1123f
Ajiva-D <davidenochaji@gmail.com>
2019-10-05 18:40:29 +0100

    added technician page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Home from './components/Home';
 import Calculator from './components/Calculator'
+import Register from './components/Register'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
 			<div className="App">
 			<Route path="/" exact component={Home} />
 			<Route path="/calculator" exact component={Calculator} />
+			<Route path="/register" exact component={Register} />
     </div>
 		</Router>
     
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "Register.css";
+import "./Register.css";
 class Register extends React.Component {
   render() {
     return (
